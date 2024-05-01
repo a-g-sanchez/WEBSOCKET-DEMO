2d6a48cf51fe6edf2bf962f2983861ff30c6f9d9
a-g-sanchez <aarongsanchez@google.com>
2024-05-01 16:54:26 -0500

    Added middleware to socket that validates that a partyId exists.
    Need to format to PH specs and validate that the userId is a member of the party they are trying to join.

--- client/src/components/messenger/Messenger.jsx
+++ client/src/components/messenger/Messenger.jsx
@@ -1,11 +1,47 @@
+import { useState } from 'react'
+import { socket } from '../../socket'
 import './messenger.css'
 
 export const Messenger = () => {
+    const [userId, setUserId] = useState(1) 
+    const [partyId] = useState(2)
+    const [message, setMessage] = useState('')
+
+    const connect = () => {
+        socket.auth = { userId, partyId }
+        socket.connect()
+    }
+
+    const sendMessage = (e) => {
+        e.preventDefault()
+        console.log(message)
+        socket.emit('send-message', message)
+        setMessage('')
+    }
+
+    const handleChange = (e) => {
+        setMessage(e.target.value)
+    }
+
+    socket.on('connect_error',(err) => {
+        console.log(err)
+    })
+
     return(
         <>
-            <seciton class="messenger-container">
+            <section className="messenger-container">
                 <p>Messenger here!</p>
-            </seciton>
+                <div>
+                    <button onClick={connect}>Connect</button>
+                    <button>Disconnect</button>
+                </div>
+                <div>
+                    <form onSubmit={sendMessage}>
+                        <input type="text" value={message} onChange={handleChange}/>
+                        <button type='submit'>Send</button>
+                    </form>
+                </div>
+            </section>
         </>
     )
 }
--- server/app.js
+++ server/app.js
@@ -1,18 +1,39 @@
 const express = require('express')
 const { createServer } = require('http')
 const { Server } = require('socket.io')
+const { parties } = require('./parties.json')
 
 const app = express()
 const httpServer = createServer(app)
-const io = new Server(httpServer)
+const io = new Server(httpServer, {cors: {
+    origin: "http://localhost:5173"
+}})
 
 app.get('/hello-world', (req, res) => {
     res.send({"msg": "Hello World"})
 })
 
+/** ------ socket implemntation ------ */
+
+// TODO refactor to PH specs.
+io.use((socket, next) => {
+    const partyId = socket.handshake.auth.partyId
+    // Validate that the partyId coming from the client exists in the array.
+    if(!parties.includes(partyId)) {
+        return next(new Error('invalid partyId'))
+    }
+    socket.partyId = partyId
+    next()
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+    // GET user id from socket.handshake.auth
+    console.log(socket.partyId)
+
+    socket.on('send-message', (data) => {
+        console.log(data)
+    })
 })
 
 module.exports ={
